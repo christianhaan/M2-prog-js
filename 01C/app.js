b591fa4846a57908d082af504141a696e2a994f4
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-11-21 15:30:20 +0100

    01C

---
+++ 01C/app.js
@@ -0,0 +1,46 @@
+class App
+{
+    runApplication()
+    {
+        class hallo1 
+        {
+            Boolean
+        }
+        
+        class hallo2
+        {
+            'hello world'
+        }
+        
+        class hallo3
+        {
+            363
+        }
+        
+        console.log("hello world!");
+        let appNaam = 'app naam = google';
+        let versienummer = 'versie nummer = 0.95345';
+        let versiedatum = new Date();
+        let auther = 'auther = christian_haan';
+        let copyright = 'copyright = leerlingen and co';
+        let distributeur = 'distributeur = leerling haan';
+        let darkmode = 'darkmode = false'
+        let test1 = Boolean
+        let test2 = 'test test test dit is een test'
+        let test3 = 32342
+        console.log("appNaam: " + app.appNaam);
+        console.log("versienummer: " + app.versienummer);
+        console.log("versiedatum: " = app.versiedatum);
+        console.log("auther: " + app.auther);
+        console.log("copyriht: " + app.copyright);
+        console.log("distributeur: " + app.distributeur);
+        console.log("darkmode " = app.darkmode);
+        console.log(test1);
+        console.log(test2);
+        console.log(test3);
+    }
+}
+
+let app = new App();
+app.runApplication();
+
