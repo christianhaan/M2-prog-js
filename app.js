b8626b4004d6754c5b208375d271d78d07076f1a
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-11-21 14:58:11 +0100

    lijst

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        //code gaat hier
+        let appNaam = 'app naam = google';
+        let versienummer = 'versie nummer = 0.95345';
+        let versiedatum = new Date();
+        let auther = 'auther = christian_haan';
+        let copyright = 'copyright = leerlingen and co';
+        let distributeur = 'distributeur = leerling haan';
+        let darkmode = 'darkmode = false'
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(auther);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
     }
 }
 
