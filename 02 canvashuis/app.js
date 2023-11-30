bdec7d2656aaf51307c5bfebf9deaabde8b2522a
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-11-30 14:57:28 +0100

    zwart_vierkantje

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -3,11 +3,10 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        let canvas = document.getElementById("canvasId");
+        let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
-        g.fillStyle = "blue";
         g.fillRect(0,0,10,10);
-        g.fillStyle = "red";
+        console.log(canvas)
     }
 }
 
