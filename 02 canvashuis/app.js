67e8ce8e6941e690a2ea1d05e2ab31d4ec87d532
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-11-30 15:07:39 +0100

    kleuren

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,6 +5,9 @@ class App
         console.log("hello world!");
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
+        g.fillStyle = "yellow";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "purple";
         g.fillRect(0,0,10,10);
         console.log(canvas)
     }
