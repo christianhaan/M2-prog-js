ed0c291020dc18b472034c36ef70d25eef8467b5
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-12-05 14:34:25 +0100

    overzichterlijker_print

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -5,7 +5,7 @@ function heeftEenResultaat()
     return resultaat;
 }
 let hetResultaat = heeftEenResultaat();
-console.log(hetResultaat);
+console.log('resultaat: '+hetResultaat);
 heeftEenResultaat();
 
 
@@ -14,7 +14,7 @@ let a =3;
 let b =4;
 let c =89;
 let y = (a*(x*x) )+ (b*x) +c;
-console.log(y);
+console.log('y: '+y);
 
 
 function ax2bcWiskunde(x,a,b,c)
@@ -24,20 +24,20 @@ function ax2bcWiskunde(x,a,b,c)
 }
 
 let y1 = ax2bcWiskunde(9,3,4,89);
-console.log(y1);
+console.log('y1: '+y1);
 
 let y2 = ax2bcWiskunde(3,6,5,45);
-console.log(y2);
+console.log('y2: '+y2);
 
 let y3 = ax2bcWiskunde(7,1,2,13);
-console.log(y3);
+console.log('y3:' +y3);
 
 
 let u = 9;
 let h = 8;
 let l = 45;
 let logY = Math.log(1) + h * Math.pow(u,2) * Math.random();
-console.log(logY);
+console.log('logY: '+logY);
 
 
 function argumentsAreHandy(shoutout)
