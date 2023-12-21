4ff2c8298ef285459c599d9baa96dd657bc5b827
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2023-12-21 14:15:56 +0100

    netjes

--- 05 arrays/5.4/app.js
+++ 05 arrays/5.4/app.js
@@ -5,18 +5,22 @@ class App{
         let array = document.getElementsByTagName("h1");
         let arrayByClass = document.getElementsByClassName("owen");
         let data = document.getElementsByTagName("h1");
-       console.log(typeof arrayByClass)
+        
+        console.log(typeof arrayByClass);
+
         arrayByClass[0].innerText = "hallo";
         arrayByClass[1].innerText = "revieuws";
         arrayByClass[2].innerText = "commentaar";
-        arrayByClass[3].innerText = "kick mastenbroek";
-        arrayByClass[4].innerText = "noordereinde 63";
+        arrayByClass[3].innerText = "chirstian haan";
+        arrayByClass[4].innerText = "jan luykenstraat 43";
  
         for (let i = 0; i < data.length; i++) {
             arrayByClass[i].innerText = i +": "+data[i].textContent;
-        }for (let i = 0; i < array.length; i++) {
+        }
+        for (let i = 0; i < array.length; i++) {
             console.log(array[i]);
-        }for (let i = 0; i < arrayByClass.length; i++) {
+        }
+        for (let i = 0; i < arrayByClass.length; i++) {
             console.log(arrayByClass[i].innerText);
         }
     }
