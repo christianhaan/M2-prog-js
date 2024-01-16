f9aa448fa184e2f1df7af8922f32e29574a0e455
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-01-16 14:28:50 +0100

    5.6

---
+++ 05 arrays/5.6 extra/app.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits);
+
+fruits.push("Kiwi", "Lemon", "Pineapple");
+console.log(fruits);
+
+fruits.splice(1, 1); 
+console.log(fruits);
