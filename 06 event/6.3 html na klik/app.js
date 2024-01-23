a70cc0b2c40e2a3f0cb10865b24a9321b27ca727
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-01-23 14:48:46 +0100

    6.3_af

--- 06 event/6.3 html na klik/app.js
+++ 06 event/6.3 html na klik/app.js
@@ -1,14 +1,26 @@
-class App
-{
-    runApplication()
-    {
-        console.log("hello world!");
 
-        uiButton.addEventListener("click", () => {
-            console.log("click!")
-        });
-    }
-}
+let uiButton = document.getElementById("uiButton");
+console.log("hello world!");
+uiButton.addEventListener("click", () => {
+    const para = document.createElement("p");
+    const node = document.createTextNode("This is new.");
+    para.appendChild(node);
+    document.body.appendChild(para);
+});
 
-let app = new App();
-app.runApplication();
+
+uiButton = document.getElementById("zelfdoen");
+console.log("hello world!");
+uiButton.addEventListener("click", () => {
+    const img = document.createElement("img");
+    const para = document.createElement("h1");
+    const p = document.createElement("p");
+    const node = document.createTextNode("IT's a me");
+    const p1 = document.createTextNode("hello world");
+    img.src="http://www.google.com/intl/en_com/images/logo_plain.png";
+    para.appendChild(node);
+    p.appendChild(p1);
+    document.body.appendChild(para);
+    document.body.appendChild(p);
+    document.body.appendChild(img)
+});
