11f1374872108cbea28125b4cd86d229dc70667c
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-02-01 12:23:22 +0100

    functions

--- 07 classes/7.1 clas/app.js
+++ 07 classes/7.1 clas/app.js
@@ -13,6 +13,7 @@ class Greet
         console.log("Greetings!");
     }
 }
+
 class Goodbye
 {
     constructor()
---
+++ 07 classes/7.2 functions/app.js
@@ -0,0 +1,62 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+    }
+}
+
+class Greet
+{
+    constructor()
+    {
+    }
+
+    showGreeting()
+    {
+        console.log("Greetings!");
+    }
+}
+
+class Goodbye
+{
+    constructor()
+    {
+    }
+
+    showBye()
+    {
+        console.log("bye bye!")
+    }
+}
+
+let greet = new Greet();
+greet.showGreeting();
+
+let bye = new Goodbye();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+greet.showGreeting();
+bye.showBye();
+
+let app = new App();
+app.runApplication();
