e34cde0b3c92f6f094fbd1a7a3ec2e403e165f5e
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-02-01 12:15:03 +0100

    classes

---
+++ 07 classes/7.1 clas/app.js
@@ -0,0 +1,27 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+    }
+}
+class Greet
+{
+    constructor()
+    {
+        console.log("Greetings!");
+    }
+}
+class Goodbye
+{
+    constructor()
+    {
+        console.log("bye bye!");
+    }
+}
+
+let greet = new Greet();
+let bye = new Goodbye();
+let app = new App();
+app.runApplication();
