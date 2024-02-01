995eb6918367cb598f43160b508a2fb216814683
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-02-01 13:40:34 +0100

    7.4

---
+++ 07 classes/7.4 dino/app.js
@@ -0,0 +1,22 @@
+class dino
+{
+    constructor(naam)
+    {
+        this.naam = naam;
+        this.vleeseter = bool;
+    }
+}
+
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        
+        let dino = new Dino();
+        console.log(dino);
+    }
+}
+
+let app = new App();
+app.runApplication();
